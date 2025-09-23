0a6b05f8dc3ccf4228ce16d8f4170cf7fc63cba7
MBonalumi <10607992@polimi.it>
2025-09-23 23:01:31 +0200

    ADDED config for frontend deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/lingo-parole-in-gioco/',
+  build: {
+    outDir: 'dist',
+  },
 })
